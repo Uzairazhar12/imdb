cf99ed7a0e65083942753187c96c8d2bde922e76
Uzairazhar12 <muhammaduzairazhar4>
2024-08-05 17:32:32 +0500

    create the navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import DarkModeSwitch from "./DarkModeSwitch";
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
-      <div className="flex gap-4">
+      <div className="flex gap-4 font-semibold">
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
